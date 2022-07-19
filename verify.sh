8d564e5048a10a785121efdab18fded9bc9371fd
NanfackSteve <nanfacksteve7@gmail.com>
2022-07-19 13:01:19 +0100

    add tools folder

--- verify.sh
+++ verify.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-function check() {
+# Dossier contenant les donnees
+datasFolder=../BUNEC_datas/var/crvs_dhis2/
+
+function checkRecords() {
     # Cette fonction verifie si oui/non les Enregistr sont completes
     # NB: On fait -1 pour ne pas compter la ligne d'en-tete
 
     # Nbre d'Enregistrement des fichiers de Synchro
-    tSync=$(grep "" -c "$1"sync_log_*)
+    tSync=$(grep "" -c "$1"/sync_log_*)
     totalSync=$((tSync - 1))
 
     # Nbre d'Enregistr. des declarations de Naiss Fosa
-    tNaissFosa=$(grep "" -c "$1"csv/formulaire_de_declaration_de_naissances_fosa_*)
+    tNaissFosa=$(grep "" -c "$1"/csv/formulaire_de_declaration_de_naissances_fosa_*)
     totalNaiss=$((tNaissFosa - 1))
 
     # Nbre d'Enregistr. des declarations de Deces Fosa
-    tDecesFosa=$(grep "" -c "$1"csv/formulaire_de_declaration_de_deces_fosa_*)
+    tDecesFosa=$(grep "" -c "$1"/csv/formulaire_de_declaration_de_deces_fosa_*)
     totalDeces=$((tDecesFosa - 1))
 
     # Nbre d'Enregistr. Total de Fosa
@@ -30,7 +33,16 @@ function check() {
 #Debut Verification
 NbrError=0
 printf "\nStart Checking...\n\n"
-for folder in */; do
-    check "$folder"
-done
+# for folder in "$datasFolder"*; do
+#     checkRecords "$folder"
+# done
 printf "\nEnd Checking. %s error's folder(s)\n\n" "$NbrError"
+
+function checkFosa() {
+    echo 'text here'
+
+}
+
+for folder in "$datasFolder"*; do
+    checkFosa "$folder"
+done
