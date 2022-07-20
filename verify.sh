7507801d8e00deb8c20d95f97588823289a2b736
NanfackSteve <nanfacksteve7@gmail.com>
2022-07-20 12:14:19 +0100

    check List FOSA - OK

--- verify.sh
+++ verify.sh
@@ -4,6 +4,7 @@
 datasFolder=../BUNEC_datas/var/crvs_dhis2/
 liste_fosa_tmp=tampon/liste_fosa.csv
 list_fosa_origin=tools/liste_fosa.csv
+log_update_fosa=tampon/update_fosa.csv
 
 function checkRecords() {
     # Cette fonction verifie si oui/non le nombre de donnees/enregistrements
@@ -34,10 +35,19 @@ function checkFosa() {
     # Cette fonction verifie les FOSA non-repertorie et les ajoutes
     # au fichier de la liste des FOSA deja repertorie
 
-    cut -d"#" -f1,14,15,16,17,18 "$1"/csv/formulaire_de_declaration_de_naissances_fosa_* | grep -a -E "^[0-9]" | sort -k 1 -n | uniq >$liste_fosa_tmp
-    grep -f $list_fosa_origin -v $liste_fosa_tmp | uniq | cat >fosa_non_rep.csv
-    UpdateNbr=$(wc -l <fosa_non_rep.csv)
-    #rm $liste_fosa_tmp
+    cut -d"#" -f1,14,15,16,17,18 "$1"/csv/formulaire_de_declaration_de_naissances_fosa_* | grep -a -E "^[0-9]" | sort -k 1 -n | uniq >>$liste_fosa_tmp
+    cat $liste_fosa_tmp | (
+        while read -r ligne; do
+            orgUnit=$(echo "$ligne" | awk -F '#' '{print $2}')
+            nbr=$(grep -c "$orgUnit" $list_fosa_origin)
+            if [ "$nbr" -lt 1 ]; then
+                # on MAJ la liste des FOSA
+                echo "$ligne" >>$list_fosa_origin
+                echo "[ Update ] $ligne" >>$log_update_fosa
+            fi
+        done
+    )
+    echo "" >$liste_fosa_tmp
 }
 
 ###################### Debut Verification ##########################
@@ -52,12 +62,11 @@ done
 printf "\n===| End Record's Number Check. %s error's folder(s) \n" "$NbrError"
 
 echo "800#dsdasdasda#test" >>$liste_fosa_tmp
-echo "900#dsdasdasda#test" >>$liste_fosa_tmp
+echo "900#dsdadasdassdasda#test" >>$liste_fosa_tmp
 UpdateNbr=0
 printf "\n---\n"
 printf "\n===| FOSA list checking... \n"
-for folder in "$datasFolder"*; do
-    checkFosa "$folder"
-done
-printf "\n===| End FOSA check checking... %s Update(s) \n" "$UpdateNbr"
+
+checkFosa "$datasFolder"2022-04-28 $UpdateNbr
+printf "\n===| End FOSA check checking...  \n"
 printf "\n----------------[ End Check ]-----------------\n\n"
